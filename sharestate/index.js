4bbfbf7e0e7a67deff0ac492819b866febb32ab8
Miguel Vásquez <angelzizu06@gmail.com>
2021-12-05 13:19:39 -0500

    :memo: ShareState
    Inmutability with literal Objects

---
+++ sharestate/index.js
@@ -0,0 +1,19 @@
+const a = {
+  value: 2,
+};
+
+// Muta
+const addOne = () => (a.value += 1);
+const timesTwo = () => (a.value *= 2);
+
+const b = {
+  value: 2,
+};
+
+// No Muta
+const addOneB = (obj) => {
+  Object.assign({}, obj, { value: obj.value + 1 });
+};
+const timesTwoB = (obj) => {
+  Object.assign({}, obj, { value: obj.value * 1 });
+};
